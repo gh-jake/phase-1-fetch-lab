8be340c9400015f4122d351b6dd711ef2c468c24
Jake Houston <jakechouston@gmail.com>
2021-03-25 13:29:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(function(json) {
+      renderBooks(json);
+    });
   
+  // return fetch('http://api.open-notify.org/astros.json')
+  //   .then(function(response) {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then(function(json) {
+  //     renderBooks(json);
+  // });
 }
 
 function renderBooks(books) {
